8b5ee2759ce9ed08f76ef2ee296281a0c80fe188
gregoadr <gregoadr@gmail.com>
2022-01-26 22:25:51 -0500

    Commit 3

--- app/src/main/java/com/example/appcocina/presentacion/LoginActivity.kt
+++ app/src/main/java/com/example/appcocina/presentacion/LoginActivity.kt
@@ -27,8 +27,7 @@ class LoginActivity : AppCompatActivity() {
         binding.btnIniciarSesion.setOnClickListener() {
             if (binding.txtEmail.text.toString().trim() == "") { //Elimino espacios en blanco
                 binding.emailField.error = getString(R.string.error)
-                println("pueba commit")
-                println("prueba 2")
+                println("commit 3")
             }
             else {
                 binding.emailField.error = null
