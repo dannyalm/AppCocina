7196ba4f0eff220188704062da0aac537ef3c84c
gregoadr <gregoadr@gmail.com>
2022-02-01 18:16:03 -0500

    Implementacion Tab Layout

--- app/src/main/java/com/example/appcocina/presentacion/PerfilFragment.kt
+++ app/src/main/java/com/example/appcocina/presentacion/PerfilFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.appcocina.controladores.ViewPagerAdapter
 import com.example.appcocina.databinding.FragmentPerfilBinding
 
 class PerfilFragment : Fragment() {
